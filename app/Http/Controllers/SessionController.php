dba3c3767531579a3b1543f2a6e7021aa1346a67
Vladimir Patrikeev <vladimir.patrikeev@rentateam.ru>
2019-01-17 15:18:28 +0300

    Added available seats check and optional id parameter in session route

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,21 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Session;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-	public function get()
+	public function get(Request $request, $id = null)
 	{
-		$sessions =  Session::where('date', '>=', Carbon::now())
-			->with('film', 'hall.cinema', 'tickets')
-			->orderBy('date', 'asc')
-			->get();
+		if (!$id) {
+			$sessions =  Session::where('date', '>=', Carbon::now())
+				->with('film', 'hall.cinema', 'tickets')
+				->orderBy('date', 'asc')
+				->get();
 
-		$available = $sessions->filter(function ($model) {
-			print($model->tickets()->count());
-			return $model->tickets()->count() < $model->hall->seats()->count();
-		});
+			$available = $sessions->filter(function ($model) {
+				print($model->tickets()->count());
+				return $model->tickets()->count() < $model->hall->seats()->count();
+			});
 
-		return $available;
+			return $available;
+		}
+		else {
+			$session = Session::with('film', 'hall.cinema', 'tickets')
+				->where('id', $id)
+				->first();
+
+			if (!$session) {
+				$request->session()->flash('status', 'No such session found!');
+				return redirect()->back();
+			}
+			else {
+				$available_seats = $session->hall->seats->filter(function ($model) use ($session) {
+					foreach($session->tickets as $ticket) {
+						if ($ticket->seat->is($model)) return false;
+					}
+					return true;
+				});
+
+				return $available_seats;
+			}
+		}
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sessions', 'SessionController@get')->name('sessions');
+Route::get('/sessions/{id?}', 'SessionController@get')->name('sessions');
