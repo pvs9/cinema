7b03ac42bf4d2443952f09c5a751b837f19e893f
Vladimir Patrikeev <vladimir.patrikeev@rentateam.ru>
2019-01-17 14:47:49 +0300

    Basic function of buying a ticket with age and seat availability checks

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTicket;
+use App\Seat;
+use App\Session;
+use App\Ticket;
 
 class TicketController extends Controller
 {
@@ -19,5 +22,29 @@ class TicketController extends Controller
 	public function buy(StoreTicket $request)
 	{
 		$data = $request->validated();
+
+		$session = Session::find($data['session']);
+		$seat = Seat::find($data['seat']);
+
+		if ($request->user()->age < $session->film->age_limit) {
+			$request->session()->flash('status', 'Your age is too low to watch this film!');
+			return redirect()->back();
+		}
+		else {
+			foreach($session->tickets as $ticket) {
+				if ($ticket->seat->is($seat)) {
+					$request->session()->flash('status', 'Your seat has been occupied!');
+					return redirect()->back();
+				}
+			}
+			$ticket = new Ticket;
+			$ticket->session()->associate($session);
+			$ticket->seat()->associate($seat);
+			$ticket->user()->associate($request->user());
+			$ticket->save();
+
+			$request->session()->flash('status', 'You have successfully bought a ticket!');
+			return redirect()->back();
+		}
 	}
 }
